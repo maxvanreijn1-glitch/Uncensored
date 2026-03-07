2758a41f898040f0ca5769b3ea0e4a809053fbfb
Max <maxvanreijn1@gmail.com>
2026-03-07 08:30:47 +0000

    create Uncensored ios SwiftUI project

---
+++ Uncensored/Uncensored/UncensoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  UncensoredApp.swift
+//  Uncensored
+//
+//  Created by Max Watson on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct UncensoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
